1a183ca498af5cced41d7542a47b3d22b0354367
Leif Åstrand <leif@vaadin.com>
2020-06-03 10:48:13 +0300

    Highlight field focused by someone else (#16)
    Fixes #8, fixes #15

--- collaboration-engine-demo/src/main/java/com/vaadin/MainView.java
+++ collaboration-engine-demo/src/main/java/com/vaadin/MainView.java
@@ -10,11 +10,13 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import com.vaadin.flow.component.HasElement;
 import com.vaadin.flow.component.HasValue;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.Push;
@@ -26,7 +28,11 @@ import com.vaadin.flow.shared.Registration;
 @Route("")
 @Push
 @CssImport("./styles/shared-styles.css")
+@JsModule("./field-collaboration.js")
 public class MainView extends VerticalLayout {
+    private static final FieldState EMPTY_FIELD_STATE = new FieldState(null,
+            Collections.emptyList());
+
     public static class Person {
         private String firstName = "";
         private String lastName = "";
@@ -56,10 +62,18 @@ public class MainView extends VerticalLayout {
 
     public static final class FieldState {
         public final Object value;
-        // TODO add list of current editors
+        public final List<String> editors;
 
-        public FieldState(Object value) {
+        public FieldState(Object value, List<String> editors) {
             this.value = value;
+            this.editors = Collections
+                    .unmodifiableList(new ArrayList<>(editors));
+        }
+
+        public FieldState(Object value, Stream<String> editors) {
+            this.value = value;
+            this.editors = Collections
+                    .unmodifiableList(editors.collect(Collectors.toList()));
         }
     }
 
@@ -125,6 +139,21 @@ public class MainView extends VerticalLayout {
                 }
             });
         }
+
+        public void updateFieldState(String fieldName,
+                Function<FieldState, FieldState> updater) {
+            updateState(state -> {
+                HashMap<String, FieldState> newStates = new HashMap<>(
+                        state.fieldStates);
+
+                FieldState oldFieldState = newStates.getOrDefault(fieldName,
+                        EMPTY_FIELD_STATE);
+
+                newStates.put(fieldName, updater.apply(oldFieldState));
+
+                return new CollaborationState(newStates, state.editors);
+            });
+        }
     }
 
     private final Binder<Person> binder = new Binder<>(Person.class);
@@ -172,10 +201,13 @@ public class MainView extends VerticalLayout {
         });
         add(avatarGroup, statusText, firstName, lastName, submitButton);
 
-        firstName.addBlurListener(
-                event -> submitValue("firstName", firstName.getValue()));
-        lastName.addBlurListener(
-                event -> submitValue("lastName", lastName.getValue()));
+        firstName.addFocusListener(event -> setEditor("firstName", username));
+        lastName.addFocusListener(event -> setEditor("lastName", username));
+
+        firstName.addBlurListener(event -> submitValue("firstName", username,
+                firstName.getValue()));
+        lastName.addBlurListener(event -> submitValue("lastName", username,
+                lastName.getValue()));
 
         /*
          * Tie subscription to submit button so that it's removed when detaching
@@ -193,14 +225,18 @@ public class MainView extends VerticalLayout {
         });
     }
 
-    private static void submitValue(String fieldName, Object value) {
-        Broadcaster.INSTANCE.updateState(state -> {
-            HashMap<String, FieldState> newStates = new HashMap<>(
-                    state.fieldStates);
+    private static void setEditor(String fieldName, String username) {
+        Broadcaster.INSTANCE.updateFieldState(fieldName, state -> {
+            return new FieldState(state.value,
+                    Stream.concat(state.editors.stream(), Stream.of(username)));
+        });
+    }
 
-            newStates.put(fieldName, new FieldState(value));
-
-            return new CollaborationState(newStates, state.editors);
+    private static void submitValue(String fieldName, String username,
+            Object value) {
+        Broadcaster.INSTANCE.updateFieldState(fieldName, state -> {
+            return new FieldState(value, state.editors.stream()
+                    .filter(editor -> !username.equals(editor)));
         });
     }
 
@@ -216,7 +252,22 @@ public class MainView extends VerticalLayout {
             binder.getBinding(fieldName).ifPresent(binding -> {
                 @SuppressWarnings("rawtypes")
                 HasValue field = binding.getField();
-                field.setValue(fieldState.value);
+                if (fieldState.value == null) {
+                    field.clear();
+                } else {
+                    field.setValue(fieldState.value);
+                }
+
+                if (field instanceof HasElement) {
+                    HasElement component = (HasElement) field;
+                    
+                    String effectiveEditor = fieldState.editors.stream()
+                            .filter(editor -> !username.equals(editor))
+                            .findFirst().orElse(null);
+                    
+                    component.getElement().executeJs(
+                            "window.setFieldState(this, $0)", effectiveEditor);
+                }
             });
         });
     }
