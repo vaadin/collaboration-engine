b5047017802267f4ab7d5352fb9ef66b023dd5d1
Pekka Maanpää <pekkamaa@vaadin.com>
2020-06-08 12:33:18 +0300

    Add collaboration-engine submodule (#47)
    Fix #41

---
+++ collaboration-engine/src/main/java/com/vaadin/collaborationengine/CollaborationEngine.java
@@ -0,0 +1,4 @@
+package com.vaadin.collaborationengine;
+
+public class CollaborationEngine {
+}
---
+++ collaboration-engine/src/test/java/com/vaadin/collaborationengine/CollaborationEngineTest.java
@@ -0,0 +1,12 @@
+package com.vaadin.collaborationengine;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CollaborationEngineTest {
+
+    @Test
+    public void dummyTest() {
+        Assert.assertTrue(true);
+    }
+}
