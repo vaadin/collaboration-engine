d1cb0d425b234a01ec6cf05f51bddb7b12dcc22b
Pekka Maanpää <pekkamaa@vaadin.com>
2020-06-10 12:27:12 +0300

    Apply formatting rules (#63)
    to fix formatting in demo module

--- collaboration-engine-demo/src/test/java/com/vaadin/AbstractViewTest.java
+++ collaboration-engine-demo/src/test/java/com/vaadin/AbstractViewTest.java
@@ -17,14 +17,15 @@ import com.vaadin.testbench.parallel.ParallelTest;
  * <p>
  * The tests use Chrome driver (see pom.xml for integration-tests profile) to
  * run integration tests on a headless Chrome. If a property {@code test.use
- * .hub} is set to true, {@code AbstractViewTest} will assume that the
- * TestBench test is running in a CI environment. In order to keep the this
- * class light, it makes certain assumptions about the CI environment (such
- * as available environment variables). It is not advisable to use this class
- * as a base class for you own TestBench tests.
+ * .hub} is set to true, {@code AbstractViewTest} will assume that the TestBench
+ * test is running in a CI environment. In order to keep the this class light,
+ * it makes certain assumptions about the CI environment (such as available
+ * environment variables). It is not advisable to use this class as a base class
+ * for you own TestBench tests.
  * <p>
  * To learn more about TestBench, visit
- * <a href="https://vaadin.com/docs/testbench/testbench-overview.html">Vaadin TestBench</a>.
+ * <a href="https://vaadin.com/docs/testbench/testbench-overview.html">Vaadin
+ * TestBench</a>.
  */
 public abstract class AbstractViewTest extends ParallelTest {
     private static final int SERVER_PORT = 8080;
@@ -56,8 +57,8 @@ public abstract class AbstractViewTest extends ParallelTest {
     }
 
     /**
-     * Convenience method for getting the root element of the view based on
-     * the selector passed to the constructor.
+     * Convenience method for getting the root element of the view based on the
+     * selector passed to the constructor.
      *
      * @return the root element
      */
@@ -67,24 +68,27 @@ public abstract class AbstractViewTest extends ParallelTest {
 
     /**
      * Asserts that the given {@code element} is rendered using a theme
-     * identified by {@code themeClass}. If the the is not found, JUnit
-     * assert will fail the test case.
+     * identified by {@code themeClass}. If the the is not found, JUnit assert
+     * will fail the test case.
      *
-     * @param element       web element to check for the theme
-     * @param themeClass    theme class (such as {@code Lumo.class}
+     * @param element
+     *            web element to check for the theme
+     * @param themeClass
+     *            theme class (such as {@code Lumo.class}
      */
-    protected void assertThemePresentOnElement(
-            WebElement element, Class<? extends AbstractTheme> themeClass) {
+    protected void assertThemePresentOnElement(WebElement element,
+            Class<? extends AbstractTheme> themeClass) {
         String themeName = themeClass.getSimpleName().toLowerCase();
-        Boolean hasStyle = (Boolean) executeScript("" +
-                "var styles = Array.from(arguments[0]._template.content" +
-                ".querySelectorAll('style'))" +
-                ".filter(style => style.textContent.indexOf('" +
-                themeName + "') > -1);" +
-                "return styles.length > 0;", element);
+        Boolean hasStyle = (Boolean) executeScript(
+                "" + "var styles = Array.from(arguments[0]._template.content"
+                        + ".querySelectorAll('style'))"
+                        + ".filter(style => style.textContent.indexOf('"
+                        + themeName + "') > -1);" + "return styles.length > 0;",
+                element);
 
-        Assert.assertTrue("Element '" + element.getTagName() + "' should have" +
-                        " had theme '" + themeClass.getSimpleName() + "'.",
+        Assert.assertTrue(
+                "Element '" + element.getTagName() + "' should have"
+                        + " had theme '" + themeClass.getSimpleName() + "'.",
                 hasStyle);
     }
 
@@ -104,15 +108,15 @@ public abstract class AbstractViewTest extends ParallelTest {
     }
 
     /**
-     * Returns whether we are using a test hub. This means that the starter
-     * is running tests in Vaadin's CI environment, and uses TestBench to
-     * connect to the testing hub.
+     * Returns whether we are using a test hub. This means that the starter is
+     * running tests in Vaadin's CI environment, and uses TestBench to connect
+     * to the testing hub.
      *
      * @return whether we are using a test hub
      */
     private static boolean isUsingHub() {
-        return Boolean.TRUE.toString().equals(
-                System.getProperty(USE_HUB_PROPERTY));
+        return Boolean.TRUE.toString()
+                .equals(System.getProperty(USE_HUB_PROPERTY));
     }
 
     /**
