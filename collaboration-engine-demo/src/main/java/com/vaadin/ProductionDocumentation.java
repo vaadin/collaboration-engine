94fc4f7fc5f5893f46078e60eca05dcf85c73310
Pekka Maanpää <pekkamaa@vaadin.com>
2020-12-15 10:54:34 +0200

    Add example of sending email from LicenseEventHandler
    I was meaning to just test that the linked "Sending email with Logback" article works, but realized that it's not the best tutorial to follow, because of e.g.:
    
    Sending email for each error log, also those not related to CE, might not be wanted
    The tutorial seemed to require some knowledge from another Logback tutorial which was not obvious
    I browsed some other examples of sending email with Java, and ended up with this example, which I decided to include as an example instead of referring to external sites.

--- collaboration-engine-demo/src/main/java/com/vaadin/ProductionDocumentation.java
+++ collaboration-engine-demo/src/main/java/com/vaadin/ProductionDocumentation.java
@@ -1,5 +1,16 @@
 package com.vaadin;
 
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message.RecipientType;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,6 +29,9 @@ import com.vaadin.flow.spring.annotation.SpringComponent;
  * Code snippets used in licensing/production documentation.
  */
 public class ProductionDocumentation extends VerticalLayout {
+
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(ProductionDocumentation.class);
 
     private UserService userService;
 
@@ -45,8 +59,56 @@ public class ProductionDocumentation extends VerticalLayout {
             System.setProperty("vaadin.ce.dataDir",
                     "/Users/steve/vaadin/collaboration-engine/");
             CollaborationEngine.getInstance().setLicenseEventHandler(event -> {
-                LOGGER.error(event.getMessage());
+                switch (event.getType()) {
+                case GRACE_PERIOD_STARTED:
+                case LICENSE_EXPIRES_SOON:
+                    LOGGER.warn(event.getMessage());
+                    break;
+                case GRACE_PERIOD_ENDED:
+                case LICENSE_EXPIRED:
+                    LOGGER.error(event.getMessage());
+                    break;
+                }
+                sendEmail(
+                        "Vaadin Collaboration Engine license needs to be updated",
+                        event.getMessage());
             });
+        }
+
+        private void sendEmail(String subject, String content) {
+            // Implement sending an email to relevant people
+        }
+
+    }
+
+    private void sendEmail(String subject, String content) {
+        // Replace the following information:
+        String from = "sender@gmail.com";
+        String password = "*****"; // Read e.g. from encrypted config file
+        String to = "receiver@gmail.com";
+        String host = "smtp.gmail.com";
+
+        Properties properties = System.getProperties();
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
+            }
+        });
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(RecipientType.TO, new InternetAddress(to));
+            message.setSubject(subject);
+            message.setText(content);
+            Transport.send(message);
+        } catch (MessagingException e) {
+            LOGGER.error(e.getMessage(), e);
         }
     }
 
