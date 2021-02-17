d5effcaf662bc53289e095863d991047b3adc125
Jens Jansson <Peppe@users.noreply.github.com>
2021-02-17 09:28:02 +0200

    Run frontend tests on mvn verify (#525)
    This installs node to collaboration-engine-frontend/field-highlighter, and runs both unit and visual tests on it, when mvn verify is run on the root folder. The visual tests add roughly 3 minutes the to test time.
    
    Fixes #124

--- collaboration-engine-frontend/field-highlighter/web-test-runner.config.js
+++ collaboration-engine-frontend/field-highlighter/web-test-runner.config.js
@@ -1,18 +1,21 @@
+const { chromeLauncher } = require('@web/test-runner');
+
 module.exports = {
   nodeResolve: true,
+  browsers: [chromeLauncher({ launchOptions: { args: ['--no-sandbox'] } })],
   coverage: true,
   coverageConfig: {
     include: ['**/src/**/*'],
     threshold: {
       statements: 99,
-      branches: 88,
+      branches: 87,
       functions: 98,
       lines: 99
     }
   },
   testFramework: {
     config: {
-      timeout: '3000',
-    },
-  },
+      timeout: '3000'
+    }
+  }
 };
