98ed996d3674801252b3e6a24d2e4ef5d743cf9d
Leif Åstrand <leif@vaadin.com>
2021-09-29 17:32:23 +0300

    doc: Document connection context implementations (#672)
    Fixes: #670 
    
    Co-authored-by: Tulio Garcia <28783969+tulioag@users.noreply.github.com>

--- collaboration-engine/src/main/java/com/vaadin/collaborationengine/ConnectionContext.java
+++ collaboration-engine/src/main/java/com/vaadin/collaborationengine/ConnectionContext.java
@@ -10,10 +10,34 @@ package com.vaadin.collaborationengine;
 
 import java.util.concurrent.Executor;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.server.VaadinService;
 import com.vaadin.flow.shared.Registration;
 
 /**
- * Defining how a topic connection should handle incoming changes.
+ * The context in which a topic connection is active. This makes it possible for
+ * a connection to become deactivate when it is no longer needed and activate it
+ * again if the context status changes. The context does also handle
+ * synchronization of date change notifications delivered to application code.
+ * <p>
+ * {@link ComponentConnectionContext} is used internally by all high-level
+ * components such as {@link CollaborationAvatarGroup} and by shorthand methods
+ * such as
+ * {@link CollaborationEngine#openTopicConnection(com.vaadin.flow.component.Component, String, UserInfo, com.vaadin.flow.function.SerializableFunction)}
+ * that take a component instance as the context. This implementation activates
+ * the topic connection whenever the target component is attached and
+ * deactivates it when the component is detached.
+ * {@link UI#access(com.vaadin.flow.server.Command)} is used for
+ * synchronization.
+ * <p>
+ * {@link SystemConnectionContext} is intended for application logic that
+ * integrates with external services that are not directly related to UI
+ * components. This context implementation is immediately active and remains
+ * active until the application is shut down (based on
+ * {@link VaadinService#addServiceDestroyListener(com.vaadin.flow.server.ServiceDestroyListener)}.
+ * Each use site (for instance each individual topic connection) gets its own
+ * synchronization to ensure events are delivered in the expected order, but
+ * still avoiding contention between other use sites.
  *
  * @author Vaadin Ltd
  * @since 1.0
