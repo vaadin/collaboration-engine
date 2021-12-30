f0bbb89b1bb2cae70cd91da913eef71dc353324d
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2021-12-30 10:55:30 +0200

    chore: update spring and field-highl version (#745)
    * chore: update spring version to latest snapshot
    
    * chore: update field-highlighter to 23.0.0-alpha2

--- collaboration-engine/src/main/java/com/vaadin/collaborationengine/CollaborationEngine.java
+++ collaboration-engine/src/main/java/com/vaadin/collaborationengine/CollaborationEngine.java
@@ -60,7 +60,7 @@ import com.vaadin.pro.licensechecker.LicenseChecker;
  * @author Vaadin Ltd
  * @since 1.0
  */
-@NpmPackage(value = "@vaadin/field-highlighter", version = "23.0.0-alpha1")
+@NpmPackage(value = "@vaadin/field-highlighter", version = "23.0.0-alpha2")
 @JsModule("@vaadin/field-highlighter/src/vaadin-field-highlighter.js")
 public class CollaborationEngine {
 
