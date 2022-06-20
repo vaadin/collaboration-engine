0ec0b5c3f13e748fc322c7958ac4c44a7ef8fa75
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2022-06-20 12:24:24 +0300

    chore: update-field-highlighter (#885)

--- collaboration-engine/src/main/java/com/vaadin/collaborationengine/CollaborationEngine.java
+++ collaboration-engine/src/main/java/com/vaadin/collaborationengine/CollaborationEngine.java
@@ -64,7 +64,7 @@ import com.vaadin.pro.licensechecker.LicenseChecker;
  * @author Vaadin Ltd
  * @since 1.0
  */
-@NpmPackage(value = "@vaadin/field-highlighter", version = "23.1.1")
+@NpmPackage(value = "@vaadin/field-highlighter", version = "23.2.0-alpha2")
 @JsModule("@vaadin/field-highlighter/src/vaadin-field-highlighter.js")
 public class CollaborationEngine {
 
